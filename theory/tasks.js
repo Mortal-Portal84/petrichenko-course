5748ae50fd95a59c471a5096b8cabfd8e9760cf9
Andrei Yeutukhou <stalker10eg@mail.ru>
2022-10-17 13:48:57 +0300

    tasks for discuss done

---
+++ theory/tasks.js
@@ -0,0 +1,8 @@
+// console.log([] + 1 + 2);
+// console.log([] + false - null + true);
+// console.log(2 && 1 && null && 0 && undefined);
+// alert(null || 2 && 3 || 4);
+
+// const a = [1, 2, 3]
+// const b = [1, 2, 3]
+// console.log(a == b);
